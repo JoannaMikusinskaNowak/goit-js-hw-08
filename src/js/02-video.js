acc7765762386f5424064ba84ab49bab6acfbd14
JoannaMikusinskaNowak <joannamikusinska@wp.pl>
2023-06-30 16:06:49 +0200

    add 8th HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,63 +6,64 @@ const player = new VimeoPlayer(iframe);
 console.log(iframe);
 console.log(player);
 
-/*player
-  .ready()
-  .then(function () {
-    console.log('Odtwarzacz Vimeo jest gotowy do użycia.');
-    // Dodaj inne operacje związane z odtwarzaczem tutaj
-  })
-  .catch(function (error) {
-    console.error('Błąd inicjalizacji odtwarzacza Vimeo:', error);
-  });*/
-
-player.on('error', function (error) {
-  console.error('Błąd odtwarzacza Vimeo:', error);
-});
-
-/* player on */
-player.on('play', function () {
-  console.log('played the video!');
-});
-/* player title */
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
 function saveTimeLocalStorage(player, delay) {
   const saveTime = function () {
-    const videoPlayerCurrentTime = player.currentTime;
-    localStorage.setItem('playerTime', 'videoPlayerCurrentTime');
-    console.log(saveTime);
+    player.getCurrentTime().then(function (currentTime) {
+      localStorage.setItem('playerTime', JSON.stringify(currentTime));
+      console.log('Zapisano czas odtwarzania:', currentTime);
+    });
   };
   const throttleSaveTime = throttle(saveTime, delay);
 
-  player.on('timeuptade', throttleSaveTime);
+  player.on('timeupdate', throttleSaveTime);
 }
 
 function setTimeThrottle(player, delay) {
-  const savedTime = localStorage.getItem('playerTime');
-  console.log(saveTimeLocalStorage);
-  if (savedTime) {
-    const seekTime = parseFloat(savedTime);
-    player
-      .setCurrentTime(seekTime)
-      .then(function (seconds) {
-        // seconds = the actual time that the player seeked to
-      })
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+  player.on('play', function () {
+    const savedTime = localStorage.getItem('playerTime');
+    console.log('Zapisano czas odtwarzania:', savedTime);
 
-          default:
-            // some other error occurred
-            break;
-        }
-      });
-  }
+    if (savedTime) {
+      const seekTime = parseFloat(savedTime);
+      player
+        .setCurrentTime(seekTime)
+        .then(function (seconds) {
+          // seconds = the actual time that the player seeked to
+        })
+        .catch(function (error) {
+          switch (error.name) {
+            case 'RangeError':
+              // the time was less than 0 or greater than the video’s duration
+              break;
+
+            default:
+              // some other error occurred
+              break;
+          }
+        });
+    }
+  });
 }
 
-saveTimeLocalStorage(player, 2000);
-setTimeThrottle(player, 2000);
+player
+  .ready()
+  .then(function () {
+    console.log('Odtwarzacz Vimeo jest gotowy do użycia.');
+    player.on('error', function (error) {
+      console.error('Błąd odtwarzacza Vimeo:', error);
+    });
+
+    /* player on */
+    player.on('play', function () {
+      console.log('played the video!');
+    });
+    /* player title */
+    player.getVideoTitle().then(function (title) {
+      console.log('title:', title);
+    });
+    saveTimeLocalStorage(player, 2000);
+    setTimeThrottle(player, 2000);
+  })
+  .catch(function (error) {
+    console.error('Błąd inicjalizacji odtwarzacza Vimeo:', error);
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.elements.email.addEventListener('input', saveFormState);
+form.elements.message.addEventListener('input', saveFormState);
+
+window.addEventListener('DOMContentLoaded', loadFormState);
+
+function saveFormState() {
+  const formState = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formState));
+}
+
+function loadFormState() {
+  const savedFormState = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedFormState) {
+    const formState = JSON.parse(savedFormState);
+
+    form.elements.email.value = formState.email;
+    form.elements.message.value = formState.message;
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const savedFormState = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedFormState) {
+    console.log('Dane formularza zostały wysłane:', JSON.parse(savedFormState));
+  }
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+});
+
+const throttleSaveFormState = throttle(saveFormState, 500);
+
+form.elements.email.addEventListener('input', throttleSaveFormState);
+form.elements.message.addEventListener('input', throttleSaveFormState);
